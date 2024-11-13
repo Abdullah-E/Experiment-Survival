e1c482bb102717d19d46dbebdc690b8735effd0f
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-11-13 22:44:20 +0500

    chase -> attack state handling

--- Assets/Scripts/DrGraves/States/Chase.cs
+++ Assets/Scripts/DrGraves/States/Chase.cs
@@ -6,6 +6,8 @@ public class Chase : BaseState
 {   
     private float losePlayerTimer;
     private float moveTimer;
+    private float attackRange = 2f;
+
     public override void Enter()
     {
         
@@ -16,7 +18,12 @@ public class Chase : BaseState
         if(controller.PlayerVisible()){
             losePlayerTimer = 0;
             controller.Agent.SetDestination(player.transform.position);
-            
+            //if distance is less than 2, attack
+            float distSquared = (controller.transform.position - player.transform.position).sqrMagnitude;
+            if(Vector3.Distance(controller.transform.position, player.transform.position) < attackRange){
+                Debug.Log("Attacking");
+                stateMachine.SwitchState(new Attack());
+            }
         }
         else{
             losePlayerTimer += Time.deltaTime;
